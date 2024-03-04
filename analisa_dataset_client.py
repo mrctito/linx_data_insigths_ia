4fb8dd41024442568e58ce1557e61304038a63f6
mrctito <mrctito.com>
2024-03-03 21:23:07 -0300

    ok

--- analisa_dataset_client.py
+++ analisa_dataset_client.py
@@ -62,11 +62,11 @@ def executar_analise_dataset():
 
         if st.button("Executar análise", key="executar_analise"):
             if query and (query is not None) and (len(query) > 0) and (st.session_state.data_frame is not None):
-                json_str = df.to_json(orient='records')
                 if opcao == 'CHAIN':
                     tabela_json_str = df.to_json(orient='table')
                     st.session_state.analise = svc_analisar_dataset_chain(tabela_json_str, query)
                 if opcao == 'PANDAS':
+                    json_str = df.to_json(orient='records')
                     st.session_state.analise = svc_analisar_dataset_pandas(json_str, query)
 
 
@@ -82,100 +82,3 @@ def executar_analise_dataset():
                 # Check if the response is an answer.
                 if "answer" in response_dict:
                     st.write(response_dict["answer"])
-
-                # Check if the response is a bar chart.
-                if "bar" in response_dict:
-                    data = response_dict["bar"]
-                    df = pd.DataFrame(data)
-                    df.set_index("columns", inplace=True)
-                    st.bar_chart(df)
-
-                # Check if the response is a line chart.
-                if "line" in response_dict:
-                    data = response_dict["line"]
-                    df = pd.DataFrame(data)
-                    df.set_index("columns", inplace=True)
-                    st.line_chart(df)
-
-                # Check if the response is a table.
-                if "table" in response_dict:
-                    data = response_dict["table"]
-                    df = pd.DataFrame(data["data"], columns=data["columns"])
-                    st.table(df)
-
-
-# versão que precisa mandar sempre o dataset
-def analisar_dados(data_frame: pd.DataFrame, query: str):
-    with st.spinner('Processando...'):
-        csv_string = data_frame.to_csv(index=False)
-
-        base_url = os.getenv('SERVER_INDEX_HTTP')
-        rota = "/api/analisar_dados"
-        url = f"{base_url}{rota}"
-
-        AnalyzeDataRequest = {
-            'data': csv_string,
-            'query': query
-        } 
-
-        token = st.session_state.auth_token
-        headers = {"Authorization": f"Bearer {token}"}
-        response = httpx.post(url, headers=headers, json=AnalyzeDataRequest, timeout=360)
-    
-        if response.status_code == 200:
-            result = response.json()
-            st.write(result)
-
-
-def preparar_dataset(data_frame: pd.DataFrame):
-    with st.spinner('Processando...'):
-
-        # Gerar JSON com os dados
-        data_json = data_frame.to_json(orient="records")
-
-        # Gerar JSON com os metadados (tipos de colunas)
-        metadata = {col: str(data_frame[col].dtype) for col in data_frame.columns}
-
-        AUTH0_M2M_CLIENT_ID = os.getenv('AUTH0_M2M_CLIENT_ID')
-
-        PrepareDatasetRequest = {
-            'client_id': AUTH0_M2M_CLIENT_ID,
-            'data': data_json,
-            'metadata': json.dumps(metadata)
-        }
-        
-        base_url = os.getenv('SERVER_INDEX_HTTP')
-        rota = "/api/preparar_dataset"
-        url = f"{base_url}{rota}"
-
-        token = st.session_state.auth_token
-        headers = {"Authorization": f"Bearer {token}"}
-        response = httpx.post(url, headers=headers, json=PrepareDatasetRequest, timeout=360)
-
-        if response.status_code == 200:
-            result = response.json()
-            dataset_id = result['dataset_id']
-            st.session_state.dataset_id = dataset_id
-            st.session_state.analise = None
-        else:
-            st.session_state.dataset_id = None
-            st.session_state.analise = None
-
-
-# versão que manda apenas o dataset_id
-def analisar_dataset(query: str):
-    with st.spinner('Processando...'):
-        base_url = os.getenv('SERVER_INDEX_HTTP')
-        rota = "/api/analisar_dataset"
-        url = f"{base_url}{rota}"
-
-        token = st.session_state.auth_token
-        headers = {"Authorization": f"Bearer {token}"}
-        params = {"id": st.session_state.dataset_id, "query": query}
-        response = httpx.get(url, headers=headers, params=params, timeout=360)
-        
-        result = None
-        if response.status_code == 200:
-            result = response.json()
-
-        return result
